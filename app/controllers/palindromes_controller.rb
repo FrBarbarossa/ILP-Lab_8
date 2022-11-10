e93effda6cc719f3e0879582d5f6b7a41f473bab
FrBarbarossa <makushinsp@mail.ru>
2022-11-10 15:11:53 +0300

    lab_8 done without test

--- app/controllers/palindromes_controller.rb
+++ app/controllers/palindromes_controller.rb
@@ -1,8 +1,15 @@
 class PalindromesController < ApplicationController
+
+
   def index
   end
 
   def result
-    @result = ['22']
+    @result = (1..params[:number].to_i).each.select{|num| if palindrome?(num.to_s) and palindrome?((num**2).to_s) then num end}.map{|i| [i, i**2]}
+  end
+
+  private 
+  def palindrome?(str)
+    str == str.reverse
   end
 end
