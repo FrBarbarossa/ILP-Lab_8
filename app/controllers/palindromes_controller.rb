afcbcd58b6b4c951bb9e6c71dc5a28160b46a8c4
FrBarbarossa <makushinsp@mail.ru>
2022-11-10 17:20:02 +0300

    Form and result-table on one page

--- app/controllers/palindromes_controller.rb
+++ app/controllers/palindromes_controller.rb
@@ -2,6 +2,7 @@ class PalindromesController < ApplicationController
 
 
   def index
+    @result = (1..params[:number].to_i).each.select{|num| if palindrome?(num.to_s) and palindrome?((num**2).to_s) then num end}.map{|i| [i, i**2]}
   end
 
   def result
