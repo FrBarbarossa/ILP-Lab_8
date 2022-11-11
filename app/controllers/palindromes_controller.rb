60924333b84bf57dbc9d204379db1c2202666e13
FrBarbarossa <makushinsp@mail.ru>
2022-11-11 23:38:04 +0300

    Lab-9 without tests

--- app/controllers/palindromes_controller.rb
+++ app/controllers/palindromes_controller.rb
@@ -1,16 +1,29 @@
+# frozen_string_literal: true
+
+# Controller for /palindromes
 class PalindromesController < ApplicationController
-
-
   def index
-    @result = (1..params[:number].to_i).each.select{|num| if palindrome?(num.to_s) and palindrome?((num**2).to_s) then num end}.map{|i| [i, i**2]}
+    @result = count_result(params[:number])
+    respond_to do |format|
+      format.html
+      format.json do
+        render json:
+          { type: @result.class.to_s, value: @result }
+      end
+    end
   end
 
   def result
-    @result = (1..params[:number].to_i).each.select{|num| if palindrome?(num.to_s) and palindrome?((num**2).to_s) then num end}.map{|i| [i, i**2]}
+    @result = count_result(params[:number])
   end
 
-  private 
+  private
+
   def palindrome?(str)
     str == str.reverse
+  end
+
+  def count_result(number)
+    (1..number.to_i).each.select { |num| if palindrome?(num.to_s) && palindrome?((num**2).to_s) then num end }.map { |i| [i, i**2] }
   end
 end
