114faea57335e290c3e0d9a715e32074b7a932c8
FrBarbarossa <makushinsp@mail.ru>
2022-11-22 10:57:16 +0300

    Tests for json works firmly

--- spec/palindromes_controller_spec.rb
+++ spec/palindromes_controller_spec.rb
@@ -23,7 +23,6 @@ RSpec.describe PalindromesController do
 end
 
 RSpec.describe PalindromesController, type: :request do
-
   describe "get index page" do
     context "check index page by root" do
       it 'has code 200' do
@@ -34,3 +33,45 @@ RSpec.describe PalindromesController, type: :request do
     end
   end
 end
+
+RSpec.describe PalindromesController, type: :controller do
+  describe "check json get" do
+    context "correct number requested" do
+      render_views
+
+      it 'has code 200' do
+        get :index, :params => {:format => :json, :number => -100 }
+        expect(response.status).to eq(200)
+      end
+      it 'returns correct json' do
+        get :index, :params => {:format => :json, :number => 100 }
+        expect(JSON.parse(response.body)["value"]).to eq([[1, 1], [2, 4], [3, 9], [11, 121], [22, 484]])
+      end
+    end
+  end
+end
+
+
+# # Попытка заполнить форму с помощью AJAX #1
+# RSpec.feature "Result table management", :type => :feature do
+#   scenario "user pushed correct number" do
+#     visit "/"
+#     fill_in "number", with: "10"
+#     click_button ("Find Palindromes")
+#     # expect(page).to have_selector("table tr")
+#   end
+# end
+
+# # Попытка заполнить форму с помощью AJAX #2
+# RSpec.describe "Result table management attempt №2",:js => true, :type => :system do
+#   before do
+#     driven_by(:rack_test)
+#   end
+
+#   it "user pushed correct number" do
+#     visit "/"
+#     fill_in "number", with: "10"
+#     click_button ("Find Palindromes")
+#     expect(page).to have_selector("table tr")
+#   end
+# end
