2207fa937aa2025db6c6b6833418fde35580e334
FrBarbarossa <makushinsp@mail.ru>
2022-11-24 11:36:07 +0300

    try to make flash from ajax

--- app/controllers/palindromes_controller.rb
+++ app/controllers/palindromes_controller.rb
@@ -2,10 +2,22 @@
 
 # Controller for /palindromes
 class PalindromesController < ApplicationController
+  # protect_from_forgery
+  before_action :check_num, only: :index
+
+
   def index
+    # redirect_to '/palindromes.js' unless flash.empty?
+    # p flash
+    redirect_to '/' unless flash.empty?
+
+    p flash[:notice]
+    return unless flash.empty?
+
     @result = count_result(params[:number])
     respond_to do |format|
-      format.html
+      format.html 
+      # format.js 
       format.json do
         render json:
           { type: @result.class.to_s, value: @result }
@@ -18,6 +30,22 @@ class PalindromesController < ApplicationController
   end
 
   private
+
+  # def clear_flash
+  #   flash.discard
+  # end
+
+  def check_num
+    number = params[:number]
+    return if number.nil?
+    if Integer(number, exception: false).nil?
+      flash[:notice] = "'#{number}' не является числом"
+      return
+    end
+    if number.to_i <= 0  then
+      flash[:notice] = "Вы ввели: '#{number}' Введите число, больше 0."
+    end
+  end
 
   def palindrome?(str)
     str == str.reverse
