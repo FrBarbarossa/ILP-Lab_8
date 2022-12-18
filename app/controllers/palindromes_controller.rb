4477966a477a5d33ca6fed88f40911cd788931fe
FrBarbarossa <makushinsp@mail.ru>
2022-12-18 14:37:45 +0300

    Little fix

--- app/controllers/palindromes_controller.rb
+++ app/controllers/palindromes_controller.rb
@@ -1,37 +1,37 @@
+# frozen_string_literal: true
+
+# Class for simple calculations
 class PalindromesController < ApplicationController
   before_action :check_num, only: :result
 
-
-  def index
-    @result = (1..params[:number].to_i).each.select{|num| if palindrome?(num.to_s) and palindrome?((num**2).to_s) then num end}.map{|i| [i, i**2]}
-  end
+  def index; end
 
   def result
     redirect_to '/' unless flash.empty?
     p flash[:notice]
     return unless flash.empty?
-    @result = count_result(params[:number].to_i)
+
+    @result = rslt.map { |i| [i, i**2] }
   end
 
-  private 
+  private
+
+  def rslt
+    (1..params[:number].to_i).each.select { |num| num if palindrome?(num.to_s) && palindrome?((num**2).to_s) }
+  end
 
   def check_num
     number = params[:number]
     return if number.nil?
+
     if Integer(number, exception: false).nil?
       flash[:notice] = "'#{number}' не является числом"
       return
     end
-    if number.to_i <= 0  then
-      flash[:notice] = "Вы ввели: '#{number}' Введите число, больше 0."
-    end
+    flash[:notice] = "Вы ввели: '#{number}' Введите число, больше 0." if number.to_i <= 0
   end
 
   def palindrome?(str)
     str == str.reverse
-  end
-
-  def count_result(number)
-    (1..number).each.select { |num| if palindrome?(num.to_s) && palindrome?((num**2).to_s) then num end }.map { |i| [i, i**2] }
   end
 end
